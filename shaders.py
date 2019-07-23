89e4317569233a5943bdc34e63bfaca5dc5165da
kevancress <kevan.cress@gmail.com>
2019-07-22 20:39:36 -0400

    Add Frag Shader AA to dashed line shader

--- shaders.py
+++ shaders.py
@@ -102,7 +102,7 @@ class Line_Shader_3D ():
             vec2 ssp1 = vec2(p1.xy / p1.w);
             vec2 ssp2 = vec2(p2.xy / p2.w);
 
-            float width = 0.00118 * thickness * aspect ;
+            float width = 0.00118 * thickness * aspect;
 
             vec2 dir = normalize(ssp2 - ssp1);
             vec2 normal = vec2(-dir[1], dir[0]);
@@ -114,6 +114,7 @@ class Line_Shader_3D ():
 
             vec4 coords[4];
             vec2 texCoords[4];
+
             coords[0] = vec4((ssp1 + offset)*p1.w,p1.z,p1.w);
             texCoords[0] = vec2(0,1);
 
@@ -187,8 +188,11 @@ class Dashed_Shader_3D ():
         in float v_ArcLength[];
         out float g_ArcLength;
 
+        uniform mat4 ModelViewProjectionMatrix;
         uniform vec2 Viewport;
         uniform float thickness;
+
+        out vec2 mTexCoord;
 
         float aspect = Viewport.x/Viewport.y;
         
@@ -201,7 +205,7 @@ class Dashed_Shader_3D ():
             vec2 ssp1 = vec2(p1.xy / p1.w);
             vec2 ssp2 = vec2(p2.xy / p2.w);
 
-            float width = 0.00118 * thickness;
+            float width = 0.00118 * thickness * aspect;
 
             vec2 dir = normalize(ssp2 - ssp1);
             vec2 normal = vec2(-dir[1], dir[0]);
@@ -211,10 +215,19 @@ class Dashed_Shader_3D ():
             offset.x /= aspect;
 
             vec4 coords[4];
-            coords[0] = vec4((ssp1 + offset),(p1.z/p1.w),1.0);
-            coords[1] = vec4((ssp1 - offset),(p1.z/p1.w),1.0);
-            coords[2] = vec4((ssp2 + offset),(p2.z/p2.w),1.0);
-            coords[3] = vec4((ssp2 - offset),(p2.z/p2.w),1.0);
+            vec2 texCoords[4];
+
+            coords[0] = vec4((ssp1 + offset)*p1.w,p1.z,p1.w);
+            texCoords[0] = vec2(0,1);
+
+            coords[1] = vec4((ssp1 - offset)*p1.w,p1.z,p1.w);
+            texCoords[1] = vec2(0,0);
+
+            coords[2] = vec4((ssp2 + offset)*p2.w,p2.z,p2.w);
+            texCoords[2] = vec2(0,1);
+
+            coords[3] = vec4((ssp2 - offset)*p2.w,p2.z,p2.w);
+            texCoords[3] = vec2(0,0);
 
             float arcLengths[4];
             arcLengths[0] = v_ArcLength[0];
@@ -223,6 +236,7 @@ class Dashed_Shader_3D ():
             arcLengths[3] = v_ArcLength[1];
 
             for (int i = 0; i < 4; ++i) {
+                mTexCoord = texCoords[i];
                 gl_Position = coords[i];
                 g_ArcLength = arcLengths[i];
                 EmitVertex();
@@ -232,6 +246,7 @@ class Dashed_Shader_3D ():
     '''
 
     fragment_shader = '''
+        in vec2 mTexCoord;
         uniform float u_Scale;
         uniform vec4 finalColor;
         
@@ -239,9 +254,25 @@ class Dashed_Shader_3D ():
         out vec4 fragColor;
 
         void main()
-        {
+        {   
+            vec4 aaColor = finalColor;
+
+            vec2 center = vec2(0,0.5);
+            float dist = length(mTexCoord - center);
+            float distFromEdge = 1-(dist*2);
+
+            float delta = fwidth(distFromEdge);
+            float threshold = 2*delta;
+            float aa = clamp((distFromEdge/threshold)+0.5,0,1);
+            aa = aa -clamp(0.5*fwidth(aa),0,1);
+
+            aaColor[3] = mix(0,1.0,aa);
+            if (finalColor[3]<0.75){
+                aaColor[3] *=0.25;
+            }
+
             if (step(sin(g_ArcLength * u_Scale), 0.5) == 1) discard;
-            fragColor = finalColor;
+            fragColor = aaColor;
         }
     '''
 
